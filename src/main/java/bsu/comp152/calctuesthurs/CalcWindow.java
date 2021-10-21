bd98f8598aebdc5ff60d93d906a771ce16fcbc26
bgravanda <bgravanda@student.bridgew.edu>
2021-10-21 10:43:39 -0400

    calc 2

--- src/main/java/bsu/comp152/calctuesthurs/CalcWindow.java
+++ src/main/java/bsu/comp152/calctuesthurs/CalcWindow.java
@@ -2,17 +2,39 @@ package bsu.comp152.calctuesthurs;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import org.controlsfx.control.action.Action;
 
 public class CalcWindow {
+    private int storedNumber;
     @FXML
     private TextField numberDisplay;
 
     @FXML
     public void numberButtonPushed(ActionEvent event){
         var currentText = numberDisplay.getText();
-        var currentNumberText = event.getSource().toString();
+        var ButtonPressed = (Button)event.getSource();
+        var currentNumberText = ButtonPressed.getText();
         var newText = currentText+currentNumberText;
+        numberDisplay.setText(newText);
+    }
+    @FXML
+    public void addButtonPressed(){
+        var numberText = numberDisplay.getText();
+        storedNumber = Integer.parseInt(numberText);
+        numberDisplay.clear();
+
+    }
+    @FXML
+    public void equalButtonPressed(){
+        var currentText =numberDisplay.getText();
+        var currentNumber = Integer.parseInt(currentText);
+        var sum = storedNumber+currentNumber;
+        numberDisplay.setText(""+sum);
+    }
+    @FXML
+    public void clearButtonPressed(){
+        numberDisplay.setText("");
     }
 }
